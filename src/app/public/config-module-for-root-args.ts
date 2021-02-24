3697638393e3d1ed2cfe56e28ee94af538bbb509
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-02-24 09:27:47 -0500

    Comments

--- src/app/public/config-module-for-root-args.ts
+++ src/app/public/config-module-for-root-args.ts
@@ -8,14 +8,8 @@ import {
 
 export interface SkyAppConfigModuleForRootArgs {
 
-  /**
-   * Blackbaud Only - Specifies configuration options for communication with SKY UX Host.
-   */
   host?: SkyuxConfigHost;
 
-  /**
-   * Specifies a list of parameters that are allowed at runtime.
-   */
   params?: SkyuxConfigParams;
 
 }
